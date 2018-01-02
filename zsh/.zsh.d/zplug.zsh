4dff466a4f8235d95895250a769684c1f0eaf201
atsushi130 <atsushi_main@outlook.jp>
2018-01-02 17:54:29 +0900

    using `defer` instead of `nice`

--- zsh/.zsh.d/zplug.zsh
+++ zsh/.zsh.d/zplug.zsh
@@ -6,7 +6,7 @@ source ~/.zplug/init.zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "b4b4r07/emoji-cli"
 
 # install zsh plugin
