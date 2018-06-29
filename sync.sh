0b8557bd3fb15c91f5c222c9d571d4dabc024bc2
atsushi130 <atsushi_main@outlook.jp>
2018-06-29 15:58:30 +0900

    add: dotman.zsh to sync

--- sync.sh
+++ sync.sh
@@ -15,6 +15,7 @@
     cp -f ~/.zsh.d/ghq.zsh           ./zsh/.zsh.d/ghq.zsh
     cp -f ~/.zsh.d/config.zsh        ./zsh/.zsh.d/config.zsh
     cp -f ~/.zsh.d/zplug.zsh         ./zsh/.zsh.d/zplug.zsh
+    cp -f ~/.zsh.d/dotman.zsh         ./zsh/.zsh.d/dotman.zsh
     cp -f ~/.sshrc                   ./.sshrc
     cp -f ~/.tmux.conf               ./tmux/.tmux.conf
     cp -f ~/.aliasrc                 ./.aliasrc
