dbe45d70df938f1a334ff12aa1c446f6186181f2
atsushi130 <atsushi_main@outlook.jp>
2018-07-01 23:56:55 +0900

    fix: hs.zsh

--- zsh/.zsh.d/hs.zsh
+++ zsh/.zsh.d/hs.zsh
@@ -1,1 +1,6 @@
-404: Not Found
+function hs-fzf() {
+  http_status_code_searcher list | fzf --ansi --reverse --prompt='status code ' | tr -d ' \\*' | xargs -n 1 http_status_code_searcher
+}
+
+zle -N hs-fzf
+bindkey '^H' hs-fzf
