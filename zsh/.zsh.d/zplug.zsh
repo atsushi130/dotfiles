8e73f8ed22ba87a19867c86f49ffe220cd9ef628
atsushi130 <atsushi_main@outlook.jp>
2019-04-15 10:31:27 +0900

    remove: emoji-cli

--- zsh/.zsh.d/zplug.zsh
+++ zsh/.zsh.d/zplug.zsh
@@ -7,7 +7,6 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "b4b4r07/emoji-cli"
 
 # install zsh plugin
 if ! zplug check --verbose; then
